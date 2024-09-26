9f67acd0826e443ceea99e7b40aad52d77441cb3
Chinmay <signups@chinmay.design>
2024-09-26 14:09:02 +1000

    Setting up netlify

--- src/pages/page_home.js
+++ src/pages/page_home.js
@@ -6,12 +6,23 @@ const pageHome = () => {
   const st_hero = new SplitType('.section.is-hero p')
 
   const tl_hero = gsap.timeline()
-  tl_hero.from(st_hero.lines, {
-    y: 15,
-    opacity: 0,
-    duration: 0.75,
-    stagger: 0.1,
-  })
+  tl_hero
+    .from(st_hero.lines, {
+      y: 15,
+      opacity: 0,
+      duration: 0.75,
+      stagger: 0.1,
+    })
+    .from(
+      '.hero-plug.is-left',
+      { x: -100, rotate: 5, opacity: 0, duration: 1.25, ease: 'power3.out' },
+      0.11
+    )
+    .from(
+      '.hero-plug.is-right',
+      { x: 100, rotate: -5, opacity: 0, duration: 1.25, ease: 'power3.out' },
+      0.115
+    )
 }
 
 export default pageHome
