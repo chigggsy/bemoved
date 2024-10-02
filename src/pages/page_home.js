bf20b5bb9a470770108a8e7f8d469867edf3da0a
Chinmay <signups@chinmay.design>
2024-10-02 13:55:11 +1000

    Carousel + Hero Animations

--- src/pages/page_home.js
+++ src/pages/page_home.js
@@ -1,40 +1,73 @@
 // import { ScrollTrigger } from 'gsap/ScrollTrigger'
-// import SplitType from 'split-type'
 import gsap from 'gsap'
 
 const pageHome = () => {
-  const testimonialMain = document.querySelector('.testimonial-main')
-  const testimonialList = document.querySelector('.testimonial-list')
-  const testimonialListParent = testimonialList.parentNode
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 992) {
-      testimonialMainMobile()
-    } else if (window.innerWidth >= 992) {
-      testimonialMainDesktop()
-    }
-  })
-
-  const testimonialMainMobile = () => {
-    testimonialListParent.insertBefore(testimonialMain, testimonialList)
+  // Functions
+  function animations() {
+    const tl_hero = gsap.timeline({ defaults: { ease: 'power3.out' } })
+    tl_hero
+      .to('.preloader', { opacity: 0, duration: 0.5 }, 0)
+      .set('.preloader', { display: 'none' }, 0.5)
+      .from('.nav-wrapper', { y: '-100%', duration: 1 }, 0)
+      .from(
+        '.u-flex-column h1:nth-child(1)',
+        { opacity: 0, y: 40, duration: 1 },
+        '>-0.5'
+      )
+      .from(
+        '.hero--highlight',
+        {
+          opacity: 0,
+          y: 40,
+          duration: 1,
+        },
+        '<+0.3'
+      )
+      // Plugs
+      .from(
+        '.hero-plug.is-left',
+        { x: -100, rotate: 10, opacity: 0, duration: 1.25 },
+        '<'
+      )
+      .from(
+        '.hero-plug.is-right',
+        { x: 100, rotate: -10, opacity: 0, duration: 1.25 },
+        '<+0.01'
+      )
+      .from('.u-flex-column p', { opacity: 0, y: 20, duration: 1 }, '<+0.15')
+      .from(
+        '.floating-action-bar',
+        { opacity: 0, y: 20, duration: 1 },
+        '<+0.05'
+      )
+      .from(
+        '.section.is-hero .benefit',
+        { opacity: 0, duration: 2, stagger: 0.03 },
+        '<+0.5'
+      )
+      .from('.marquee', { opacity: 0, duration: 1 }, '<')
+      .from('.section.is-steps', { opacity: 0, duration: 1 }, '<')
   }
-  const testimonialMainDesktop = () => {
-    testimonialListParent.insertBefore(testimonialList, testimonialMain)
+
+  function accordion() {
+    const accordionList = document.querySelectorAll('.faq')
+
+    accordionList.forEach((accordion) => {
+      accordion.addEventListener('click', () => {
+        const answer = accordion.querySelector('.faq-answer')
+        const chevron = accordion.querySelector('.faq-chevron')
+
+        answer.classList.toggle('is-open')
+        if (chevron) {
+          chevron.classList.toggle('is-active')
+        }
+      })
+    })
   }
 
-  // Animation
-  const tl_hero = gsap.timeline()
-  tl_hero
-    .from(
-      '.hero-plug.is-left',
-      { x: -100, rotate: 5, opacity: 0, duration: 1.25, ease: 'power3.out' },
-      0.11
-    )
-    .from(
-      '.hero-plug.is-right',
-      { x: 100, rotate: -5, opacity: 0, duration: 1.25, ease: 'power3.out' },
-      0.115
-    )
+  // Runs
+  animations()
+  accordion()
 }
 
 export default pageHome
